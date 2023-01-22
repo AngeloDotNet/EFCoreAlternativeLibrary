acbf8c4b1b8f62a751a2778d1bc7a1ae5d079738
Angelo Pirola <angelo9871@gmail.com>
2023-01-22 22:37:36 +0100

    Aggiungere i file di progetto.

---
+++ Data.EFCore.Generic/Command.cs
@@ -0,0 +1,43 @@
+﻿using Data.EFCore.Generic.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.EFCore.Generic;
+
+public class Command<T> : ICommand<T> where T : class
+{
+    private readonly DbContext _dbContext;
+    private readonly DbSet<T> _set;
+
+    public Command(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _set = _dbContext.Set<T>();
+    }
+
+    public async Task CreateAsync(T entity)
+    {
+        _set.Add(entity);
+
+        await _dbContext.SaveChangesAsync();
+
+        _dbContext.Entry(entity).State = EntityState.Detached;
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _set.Update(entity);
+
+        await _dbContext.SaveChangesAsync();
+
+        _dbContext.Entry(entity).State = EntityState.Detached;
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        _set.Remove(entity);
+
+        await _dbContext.SaveChangesAsync();
+
+        _dbContext.Entry(entity).State = EntityState.Detached;
+    }
+}
---
+++ Data.EFCore.Generic/Database.cs
@@ -0,0 +1,35 @@
+﻿using Data.EFCore.Generic.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.EFCore.Generic;
+
+public class Database<T> : IDatabase<T> where T : class
+{
+    private readonly DbContext _dbContext;
+    private readonly DbSet<T> _set;
+
+    public Database(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _set = _dbContext.Set<T>();
+    }
+
+    public Task<List<T>> GetAllAsync()
+    {
+        return _set.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        var entity = await _set.FindAsync(id);
+
+        if (entity == null)
+        {
+            return null;
+        }
+
+        _dbContext.Entry(entity).State = EntityState.Detached;
+
+        return entity;
+    }
+}
